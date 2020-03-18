a2ce8b0df8c43ed2e666b5d2c3ddacda04dd9e00
Keyanna Xu <xkeyanna@yahoo.com>
2020-03-17 20:16:48 -0700

    fetching json with okhttp and gson

--- app/src/main/java/com/example/kotlinyoutube/MainActivity.kt
+++ app/src/main/java/com/example/kotlinyoutube/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.kotlinyoutube
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.Color
+import android.provider.MediaStore
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +19,57 @@ class MainActivity : AppCompatActivity() {
 //        recyclerView_main.setBackgroundColor(Color.BLUE)
 
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-        recyclerView_main.adapter = MainAdapter()
+//        recyclerView_main.adapter = MainAdapter()
+
+        fetchJson()
     }
+    fun fetchJson() {
+        println("Attempting to fetch JSON")
+
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+                runOnUiThread {
+                    recyclerView_main.adapter = MainAdapter(homeFeed)
+                }
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+
+        })
+    }
+
 }
+
+//{
+//    "id": 1,
+//    "name": "Instagram Firebase - Learn How to Create Users, Follow, and Send Push Notifications",
+//    "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
+//    "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93",
+//    "numberOfViews": 20000,
+//    "channel": {
+//    "name": "Lets Build That App",
+//    "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
+//    "numberOfSubscribers": 100000
+//}
+//},
+
+class HomeFeed(val videos: List<MediaStore.Video>)
+
+class Video(val id: Int, val name: String, val link: String, val imageUrl: String,
+            val numberOfViews: Int, val channel: Channel)
+
+class Channel(val name: String, val profileImageUrl: String)
--- app/src/main/java/com/example/kotlinyoutube/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinyoutube/MainAdapter.kt
@@ -6,17 +6,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.video_row.view.*
 
-class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
 
     val videoTitles = listOf("First title", "Second", "3rd", "Moore titles")
 
     override fun getItemCount(): Int {
-        return videoTitles.size
+//        return videoTitles.size
+        return homeFeed.videos.count()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         //How do we create a view?
-        val layoutInflater = LayoutInflater.from(parent?.context)
+        val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
         return CustomViewHolder(cellForRow)
     }
