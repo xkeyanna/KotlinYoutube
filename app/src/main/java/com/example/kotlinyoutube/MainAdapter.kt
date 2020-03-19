af7c23ba25f884709245a1c2f8df35a8dd742467
Keyanna Xu <xkeyanna@yahoo.com>
2020-03-19 15:28:27 -0700

    picasso images

--- app/src/main/java/com/example/kotlinyoutube/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinyoutube/MainAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
@@ -26,6 +27,14 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
 //      val videoTitle = videoTitles.get(position)
         val video = homeFeed.videos.get(position)
         holder.view.textView_video_title.text = video.name
+
+        holder.view.textView_channel_name.text = video.channel.name + " ~ " + "20K Views\n4 days ago"
+
+        val thumbnailImageView = holder.view.imageView_video_thumbnail
+        Picasso.with(holder?.view?.context).load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = holder.view.imageView_channel
+        Picasso.with(holder.view.context).load(video.channel.profileImageUrl).into(channelProfileImageView)
     }
 }
 
